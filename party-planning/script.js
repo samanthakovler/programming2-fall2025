b3f95a5fd3c8a556828849daae83d0d2dff058d3
Samantha Kovler <samantha@Samanthas-MacBook-Air.local>
2025-12-02 14:27:58 -0600

    party

---
+++ party-planning/script.js
@@ -0,0 +1,31 @@
+function party() {
+	const people = document.getElementById("people").value;
+	let sodas = (people * 2);
+	let snacks = (people * 3);
+	let balloons = (people * 4);
+	let chairs = (people + 3);
+	let partyhat = (people + 3);
+
+
+
+	const result= "You will need: ";
+	sodas = sodas + " sodas";
+	snacks = snacks + " snacks";
+	balloons = balloons + " balloons";
+	chairs = chairs + " chairs";
+	partyhat = partyhat + " party hats";
+	document.getElementById("result").innerText = result;
+	document.getElementById("sodas").innerText = sodas;
+	document.getElementById("snacks").innerText = snacks;
+	document.getElementById("balloons").innerText = balloons;
+	document.getElementById("chairs").innerText = chairs;
+	document.getElementById("partyhat").innerText = partyhat;
+	const sound = new Audio("surprise.mp3");
+			sound.play();
+			document.body.style.backgroundColor = "Chartreuse";
+
+
+
+
+
+}
