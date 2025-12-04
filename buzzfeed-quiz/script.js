5f0bbd7a0d935b6aec04b4d92c7029bc6690b4fd
Samantha Kovler <samantha@Samanthas-MacBook-Air.local>
2025-12-04 12:42:04 -0600

    buzfeed
    clear
    . clear
    .. clear

---
+++ buzzfeed-quiz/script.js
@@ -0,0 +1,100 @@
+let mike = 0;
+let sully = 0;
+let roz = 0;
+let randall = 0;
+let boo = 0;
+
+
+function getResults() {
+	let chaotic = document.getElementById('chaotic');
+	if(chaotic.checked) { // if filled in
+		console.log("The user chose chaotic");
+		mike = Number(mike) + 1
+	}
+
+	let loyal = document.getElementById('loyal');
+	if(loyal.checked) {
+		console.log("The user chose loyal");
+		sully = Number(sully) + 1
+	}
+
+	let sarcastic = document.getElementById('sarcastic');
+	if(sarcastic.checked) {
+		console.log("The user chose sarcastic");
+		roz = Number(roz) + 1
+	}
+
+		let clever = document.getElementById('clever');
+	if(clever.checked) {
+		console.log("The user chose clever");
+		randall = Number(randall) + 1
+	}
+
+		let funny = document.getElementById('funny');
+	if(funny.checked) {
+		console.log("The user chose funny");
+		boo = Number(boo) + 1
+	}
+
+//fears
+
+	let dissapoint = document.getElementById('dissapoint');
+	if(dissapoint.checked) { // if filled in
+		console.log("The user chose dissapoint");
+		sully = Number(sully) + 1
+	}
+
+	let bugs = document.getElementById('bugs');
+	if(bugs.checked) {
+		console.log("The user chose bugs");
+		boo = Number(boo) + 1
+	}
+
+	let failure = document.getElementById('failure');
+	if(failure.checked) {
+		console.log("The user chose failure");
+		mike = Number(mike) + 1
+	}
+		let noises = document.getElementById('noises');
+	if(noises.checked) {
+		console.log("The user chose noises");
+		roz = Number(roz) + 1
+	}
+
+	let nothing = document.getElementById('nothing');
+	if(nothing.checked) {
+		console.log("The user chose nothing");
+		randall = Number(randall) + 1
+	}
+
+//job
+
+	let dissapoint = document.getElementById('dissapoint');
+	if(dissapoint.checked) { // if filled in
+		console.log("The user chose dissapoint");
+		sully = Number(sully) + 1
+	}
+
+	let bugs = document.getElementById('bugs');
+	if(bugs.checked) {
+		console.log("The user chose bugs");
+		boo = Number(boo) + 1
+	}
+
+	let failure = document.getElementById('failure');
+	if(failure.checked) {
+		console.log("The user chose failure");
+		mike = Number(mike) + 1
+	}
+		let noises = document.getElementById('noises');
+	if(noises.checked) {
+		console.log("The user chose noises");
+		roz = Number(roz) + 1
+	}
+
+	let nothing = document.getElementById('nothing');
+	if(nothing.checked) {
+		console.log("The user chose nothing");
+		randall = Number(randall) + 1
+	}
+}
